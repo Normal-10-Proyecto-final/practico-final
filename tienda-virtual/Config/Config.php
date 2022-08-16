cfc763f7be4bcaa335d7f8d188eb635c2fd4a160
Juancho <105228069+Juancho008@users.noreply.github.com>
2022-08-16 16:48:42 -0300

    Add files via upload

---
+++ tienda-virtual/Config/Config.php
@@ -0,0 +1,11 @@
+<?php
+const BASE_URL = "http://localhost/tienda-virtual/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda_virtual";
+const CHARSET = "charset=utf8";
+const TITLE = "Ozymandias";
+const MONEDA = "ARS";
+const CLIENT_ID = "";
+?>
